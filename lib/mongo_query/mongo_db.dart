5f9620d5fd18ad809c45c511981e85c108be4f2e
hiren <hiren@byteztech.in>
2023-04-28 18:12:47 +0530

    insert

--- lib/mongo_query/mongo_db.dart
+++ lib/mongo_query/mongo_db.dart
@@ -3,6 +3,8 @@ import 'dart:developer';
 import 'package:mongo_dart/mongo_dart.dart';
 import 'package:mongo_db_practice/utils/constants.dart';
 
+import '../model/mongo_model.dart';
+
 class MongoDatabase {
 
   static var db, userCollection;
@@ -15,5 +17,19 @@ class MongoDatabase {
     inspect(db);
     userCollection = db.collection(mongoCollectionName);
     print("object = > ${userCollection}");
+  }
+
+  /// Insert into database
+  static Future<String> insertMongo (MongoData data) async {
+    try{
+      var result = await userCollection.insertOne(data.toJson());
+      if(result.isSuccess){
+        return "succefull";
+      }else{
+        return "unsuccefull";
+      }
+    }catch (e){
+      return e.toString();
+    }
   }
 }
